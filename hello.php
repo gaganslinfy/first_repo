7ddee40044cfb98da8a9aa055ef4cdd3d010c48b
admin2-PC\admin2 <gagandeep.singh02@slinfy.com>
2016-09-01 15:40:49 +0530

    new code

---
+++ hello.php
@@ -0,0 +1,16 @@
+<h1>test</h1>
+
+<form name="testform">
+            <div class="contact-form">
+              <label> <span>Full name</span>
+              <input type="text" class="input_text" name="name" id="name" ng-model="name"/>
+              </label>
+              <label> <span>About Me</span>
+              <textarea class="message" name="about" id="about" ng-model="about"></textarea>
+   
+              <label> <span>Password</span>
+              <input type="password" class="input_text" name="password" id="password" ng-model="password"/>
+              <input type="submit" class="button" value="Submit Form"  ng-click="submit(); loadData();"/>
+              </label>
+            </div>
+          </form>
